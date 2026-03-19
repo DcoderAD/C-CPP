cb8f5c8edd86fd480d2d098f7a53bb2200123e57
ajay <er.ajycse@gmail.com>
2026-03-19 20:35:04 +0530

    notes

---
+++ cpp/add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int a,b;
+    std::cout<<"Enter value of a,b:";
+    std::cin >>a>>b;
+    int sum=a+b;
+    std::cout<<"sum of "<<a<<" and "<<b<<" is "<<sum<<std::endl;
+    return 0;
+}
---
+++ cpp/p1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    std::cout << "hello this is c++";
+    std::cout << std::endl;
+    return 0;
+}
